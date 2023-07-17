5dc86d3f247796c883e4facec4028e8f82e08d74
Perla Arroyos <perla.pmar04@gmail.com>
2023-07-17 00:11:28 -0600

    Add project

---
+++ src/main/java/com/perlaarroyos/ApplicationFacesConfig.java
@@ -0,0 +1,10 @@
+package com.perlaarroyos;
+
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.faces.annotation.FacesConfig;
+
+@FacesConfig
+@ApplicationScoped
+public class ApplicationFacesConfig {
+}
